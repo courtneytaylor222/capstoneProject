d3f7c8621584fc42d8d15ba9b246992d99bc3b65
Courtney Taylor <courtneytaylor222@outlook.com>
2025-05-06 18:39:21 +0100

    Edited the tasks.json file, still getting errors, not managed to fix yet

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <boost/asio.hpp>         // Core Boost.Asio networking
+#include <boost/asio/ssl.hpp>     // SSL/TLS support for Boost.Asio
+#include <string>
+#include <vector>
+
+
+int main()
+{
+    
+    return 0;
+}
